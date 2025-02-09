1a88a2b2a7211ca9f2e3860b90ebdc7dd2136d94
noName <samyrize77777@gmail.com>
2025-02-10 02:35:29 +0400

    Deploy Flask with MySQL on Railway

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URL', 'mysql://root:DmRAFbxJAuoroDekTYiWQBiTMndpFcXr@junction.proxy.rlwy.net:32757/railway'
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
