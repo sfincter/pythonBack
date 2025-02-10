a9be57138d88de311b6b44ca58769c954548e150
noName <samyrize77777@gmail.com>
2025-02-10 19:28:43 +0400

    fix req

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
-        'DATABASE_URL', 'mysql://root:DmRAFbxJAuoroDekTYiWQBiTMndpFcXr@junction.proxy.rlwy.net:32757/railway'
+        "DATABASE_URL", "mysql+pymysql://root:DmRAFbxJAuoroDekTYiWQBiTMndpFcXr@junction.proxy.rlwy.net:32757/railway"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
