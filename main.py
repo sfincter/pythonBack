df1bbc5db392b344bce43beb0386f5f272f65946
noName <samyrize77777@gmail.com>
2025-02-12 20:25:20 +0400

    jsonify

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
 import logging
